7d83fd8455fa9a1eb7c274a8306c59b1e3ab3fc6
Sahil Rochwani <hawabaji236@gmail.com>
2026-06-11 17:38:56 +0000

    Added variables and datatypes examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 12445
+let accountEmail = "sahil@google.com"
+var accountPassword = "1345"
+
+accoountCity = "Jaipur" // we can also allocate memory without using keyword but this is not good
+//if we not assign any value in variable so it gives undefined
+
+let accountState; //semicolon is not necessary in js
+
+
+// accountId = 2 // this is not allowed because it already fixed with const keyword
+
+accountEmail = "sr@google.com"
+accountPassword = "953742"
+accoountCity = "Bengaluru"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountId,accountPassword,accoountCity,accountState])// this will create a table of all in the bracket
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //this will treat all js code as newer version
+
+// alert(3+3) // this will work browser console but it gives error in nodejs
+
+// console.log(3+3) console.log("sahil") //this give error,if we use ; after first console then it is correct but that will reduce readability of the code
+
+console.log(3 + 3);
+console.log("sahil");
+
+//types of datatypes
+let name = "sahil"
+let age = 20
+let isLoggenIn = true
+
+// null = it is a standalone value
+// undefined = in this we cannot assign any value
+//symbol = it is used for uniqness
+
+console.log(typeof "sahil")
+console.log(typeof age)
+console.log(typeof null) // it gives its type is object 
+console.log(typeof undefined)
